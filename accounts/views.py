d378e25caad52c559b30dd620964c84fd8e4d070
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-06 16:36:08 +0430

    register views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from accounts.models import CustomerUser # If used custom user model
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+
+    model = CustomerUser
+    permission_classes = [
+        permissions.AllowAny # Or anon users can't register
+    ]
+    serializer_class = UserSerializer
