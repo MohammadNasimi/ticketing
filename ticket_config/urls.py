ccc869e43db5d83077ddc0a35811846bc866b2d2
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-06 17:15:18 +0430

    add swagger url

--- ticket_config/urls.py
+++ ticket_config/urls.py
@@ -15,8 +15,45 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework import permissions 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi 
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
+  def get_schema(self, request=None, public=False):
+    """Generate a :class:.Swagger object with custom tags"""
+
+    swagger = super().get_schema(request, public)
+    swagger.tags = [
+        {
+            "name": "ticketing",
+            "description": "ticketing API"
+        },
+    ]
+
+    return swagger
+
+schema_view = get_schema_view( 
+openapi.Info(
+title="Blog API",
+default_version="v1",
+description="A sample API for learning DRF",
+terms_of_service="https://www.google.com/policies/terms/",
+contact=openapi.Contact(email="hello@example.com"),
+license=openapi.License(name="BSD License"),
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls')),
+    path('ticketing/',include('ticketing.urls')),
+    path('swagger/', schema_view.with_ui( 
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui( 
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
