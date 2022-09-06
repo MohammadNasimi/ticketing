83724bbbcd349ca3631d413a7f9bcd0e0cf90364
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-06 16:35:57 +0430

    register url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from rest_framework_simplejwt import views as jwt_views
+from accounts.views import CreateUserView
 urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='login'),
-    path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login/refresh/'),
+    path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
+    path('register/',CreateUserView.as_view(),name='register')
 ]
