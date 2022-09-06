8d2ad781abcbe4462a2a1ff40a302c5ada3b7e2f
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-06 16:35:44 +0430

    register serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from accounts.models import CustomerUser # If used custom user model
+
+UserModel = CustomerUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+
+        user = UserModel.objects.create(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = UserModel
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
