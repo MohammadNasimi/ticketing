022afb3fff9fac245df8b7b97dcb2d3cbf9c4ac7
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-06 17:16:26 +0430

    add model

---
+++ ticketing/models.py
@@ -0,0 +1,30 @@
+from operator import mod
+from django.db import models
+from accounts.models import CustomerUser
+# Create your models here.
+Choices_type =(
+    ( '1','Financial'),
+    ('2','Social'),
+    ('3','holiday'),
+    ('4','Others')
+)
+class Ticket(models.Model):
+    title = models.CharField(max_length=20)
+    auther = models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    text = models.TextField()
+    date =models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=2,choices = Choices_type, default= '4')
+    def __str__(self) -> str:
+        return self.title
+    
+
+
+class TicketAnswer(models.Model):
+    question = models.ForeignKey(Ticket,on_delete=models.CASCADE)
+    auther = models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    text = models.TextField()
+    date =models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self) -> str:
+        return self.question.title
