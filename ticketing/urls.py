c5015427ec3c375cc74693197de0054e842c4116
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-07 11:10:35 +0430

    change name url

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
 from ticketing.views import CreateQuestionView,CraetaAnswerView,UpdateTicktetView,UpdateTicktetAnswerView
 urlpatterns = [
-    path('Ticket/list/',CreateQuestionView.as_view(),name = 'create_question'),
-    path('TicketAnswer/list/',CraetaAnswerView.as_view(),name = 'create_answer'),
-    path('Ticket/detail/<int:pk>/',UpdateTicktetView.as_view(),name = 'detail_question'),
-    path('TicketAnswer/detail/<int:pk>/',UpdateTicktetAnswerView.as_view(),name = 'detail_Answer')
+    path('ticket/list/',CreateQuestionView.as_view(),name = 'create_question'),
+    path('ticketAnswer/list/',CraetaAnswerView.as_view(),name = 'create_answer'),
+    path('ticket/detail/<int:pk>/',UpdateTicktetView.as_view(),name = 'detail_question'),
+    path('ticketAnswer/detail/<int:pk>/',UpdateTicktetAnswerView.as_view(),name = 'detail_Answer')
 ]
