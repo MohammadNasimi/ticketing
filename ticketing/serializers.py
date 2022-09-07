b0a35a008a893d6eb67638ac07d263d5047b650b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-07 11:10:20 +0430

    add nested user in ser

--- ticketing/serializers.py
+++ ticketing/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from ticketing.models import Ticket,TicketAnswer
-
+from accounts.serializers import UserSerializer
 class QuestionTicketSerializer(serializers.ModelSerializer):
+    auther = UserSerializer(read_only=True)
+
     class Meta:
         fields = ['id','title' , 'auther','text', 'date','type']
         model = Ticket
         read_only_fields =['id','auther','date']
 class AnswerTicketSerializer(serializers.ModelSerializer):
+    auther = UserSerializer(read_only=True)
     class Meta:
         fields = ['id','question','auther','text','date']
         model = TicketAnswer
