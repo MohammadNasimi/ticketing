a80315d6268f8991637cd77659165076d4a978f5
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-07 13:02:57 +0430

    secret Key add

--- ticket_config/settings.py
+++ ticket_config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2#0c$r@n2b!1lu8_me#drm7=p42s_u22fmwz^rxek3i6flz1+0'
+# SECRET_KEY = 'django-insecure-2#0c$r@n2b!1lu8_me#drm7=p42s_u22fmwz^rxek3i6flz1+0'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
