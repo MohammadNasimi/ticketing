6a23d561de6db8b430958ab6ce73b8f639b35665
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-08 15:41:31 +0430

    custom login jwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path 
 from rest_framework_simplejwt import views as jwt_views
-from accounts.views import CreateUserView
+from accounts.views import CreateUserView ,MyTokenObtainPairView
 urlpatterns = [
-    path('login/', jwt_views.TokenObtainPairView.as_view(), name='login'),
+    path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
     path('register/',CreateUserView.as_view(),name='register')
 ]
