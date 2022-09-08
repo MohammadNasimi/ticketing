32760136f197d6c8b45ae1ec414e35dc0616ad57
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-08 15:42:00 +0430

    ser login for custom jwt

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -28,3 +28,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
         write_only_fields = ('password',)
         read_only_fields = ('id',)
+        
+
+class LoginSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = UserModel
+        fields = ('id', 'username', 'password','email', 'first_name', 'last_name')
+        read_only_fields = ('id','email', 'first_name', 'last_name')
