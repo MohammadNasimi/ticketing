0ed9c4de75a149ed91557bd7e59e57ed10a5d1db
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-10 15:48:13 +0430

    add serializer list ticketAnswerfor Ticket

--- ticketing/serializers.py
+++ ticketing/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from ticketing.models import Ticket,TicketAnswer
 from accounts.serializers import UserSerializer
-class QuestionTicketSerializer(serializers.ModelSerializer):
-    auther = UserSerializer(read_only=True)
 
-    class Meta:
-        fields = ['id','title' , 'auther','text', 'date','type']
-        model = Ticket
-        read_only_fields =['id','auther','date']
 class AnswerTicketSerializer(serializers.ModelSerializer):
     auther = UserSerializer(read_only=True)
+    
     class Meta:
         fields = ['id','question','auther','text','date']
         model = TicketAnswer
         read_only_fields =['id','auther','date']
+        
+class QuestionTicketSerializer(serializers.ModelSerializer):
+    auther = UserSerializer(read_only=True)
+    ticket_answer_obj = serializers.SerializerMethodField('get_ticket_answer_obj')
+
+    class Meta:
+        fields = ['id','title' , 'auther','text', 'date','type','ticket_answer_obj']
+        model = Ticket
+        read_only_fields =['id','auther','date','ticket_answer_obj']
+        
+    
+    def get_ticket_answer_obj(self, obj):
+        results = TicketAnswer.objects.filter(question = obj.id)
+        return AnswerTicketSerializer(results, many=True).data
