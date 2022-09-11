ddda9e4d0457ab11ffcd23d60fdd73ed6134d899
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-11 11:49:29 +0430

    ticketanswer/list/<int:pk> change

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from ticketing.views import CreateQuestionView,CraetaAnswerView,UpdateTicktetView,UpdateTicktetAnswerView
 urlpatterns = [
     path('ticket/list/',CreateQuestionView.as_view(),name = 'create_question'),
-    path('ticketanswer/list/',CraetaAnswerView.as_view(),name = 'create_answer'),
+    path('ticketanswer/list/<int:pk>/',CraetaAnswerView.as_view(),name = 'create_answer'),
     path('ticket/detail/<int:pk>/',UpdateTicktetView.as_view(),name = 'detail_question'),
     path('ticketanswer/detail/<int:pk>/',UpdateTicktetAnswerView.as_view(),name = 'detail_Answer')
 ]
