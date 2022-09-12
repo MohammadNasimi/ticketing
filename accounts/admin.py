83e44e691235f67ae35df7fc574c771ac949291a
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-12 13:26:04 +0430

    change USer to customer admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,11 @@
-import imp
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import CustomerUser
+from accounts.models import User ,Customer ,Admin
 # Register your models here.
 
  
-class CustomerUserAdmin(UserAdmin):
-    model = CustomerUser
+class UserAdmin(UserAdmin):
+    model = User
     list_display = ['email', 'username', 'type', 'is_staff', ] 
     fieldsets = (
         ('General' , {'fields':['email', 'username',  'is_staff', ]}),
@@ -17,4 +16,6 @@ class CustomerUserAdmin(UserAdmin):
     list_filter = ['email', 'username', 'type', 'is_staff']
     search_fields = ['email', 'username', 'type', 'is_staff']
 
-admin.site.register(CustomerUser,CustomerUserAdmin)
+admin.site.register(User,UserAdmin)
+admin.site.register(Customer)
+admin.site.register(Admin)
