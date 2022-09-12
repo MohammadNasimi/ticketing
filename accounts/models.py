4fc538b5793cdf8186b193e815dfd571a99f48fa
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-09-12 13:26:20 +0430

    add admin customer model

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,34 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+
+from ticket_config.settings import AUTH_USER_MODEL
 Choices =(
     ('1','admin'),
     ('2','customer')
 )
-class CustomerUser(AbstractUser):
-    type = models.CharField( max_length=5,choices=Choices,default='2')
+class User(AbstractUser):
+    type = models.CharField( max_length=5,choices=Choices,default='1')
+    
+
+class profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    class Meta:
+        abstract = True
+
+class Customer(profile):
+    phone = models.CharField(max_length=11)
+    def __str__(self) -> str:
+        return self.phone
+    
+class Admin(profile):
+    phone = models.CharField(max_length=11)
+    def __str__(self) -> str:
+        return self.phone
+
+
+    
